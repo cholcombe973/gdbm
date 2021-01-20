075a8924e83553c95342ad3215e0c58ac1e97c87
Miquel van Smoorenburg <mikevs@xs4all.net>
2021-01-20 20:51:37 +0100

    fix test

--- tests/test.rs
+++ tests/test.rs
@@ -10,12 +10,12 @@ fn create_test() {
     // Should create a dbm
     let result = gdbm::Gdbm::new(Path::new("test"),
                                  0,
-                                 gdbm::NEWDB,
+                                 gdbm::Open::NEWDB,
                                  (S_IRUSR | S_IWUSR) as i32)
         .unwrap();
     // Lets write a key/value and then read it back
     let mut data = "blah".to_string();
-    let store_result = result.store("foo", &mut data, gdbm::INSERT);
+    let store_result = result.store("foo", &mut data, gdbm::Store::INSERT);
     assert_eq!(store_result, 0);
     let fetch_result = result.fetch("foo").unwrap();
     assert_eq!("blah".to_string(), fetch_result);
